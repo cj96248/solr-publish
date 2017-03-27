ff39d751c8740e130ffc6f0443a493d580c0f202
cj96248 <253037585@qq.com>
2017-03-27 23:23:35 +0800

    Add query parse

--- src/main/java/com/trainning/project/query/SimpleQuery.java
+++ src/main/java/com/trainning/project/query/SimpleQuery.java
@@ -11,4 +11,9 @@ import org.apache.solr.search.SyntaxError;
 * @date 2017年3月27日下午10:53:09
 */
 public class SimpleQuery {
+    public void parse(QParser analyzer) throws SyntaxError{
+        QueryParser parser = new QueryParser(Version.LUCENE_6_0_0,"body", analyzer);
+        Query bodyQuery = parser.parse("IBM");
+        
+    }
 }
